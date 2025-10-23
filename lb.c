2611dfa86704fb5af7b85ee1867b4c8c952eb22e
Teodor Janez Podobnik <48418580+dorkamotorka@users.noreply.github.com>
2025-10-23 23:04:24 +0200

    Swap source and destination ports in lb.c

--- lb.c
+++ lb.c
@@ -195,8 +195,8 @@ int xdp_load_balancer(struct xdp_md *ctx) {
   struct four_tuple_t in;
   in.src_ip = ip->daddr;     // Load Balancer IP
   in.dst_ip = ip->saddr;     // Client or Backend IP
-  in.src_port = tcp->source; // Client or Backend source port
-  in.dst_port = tcp->dest;   // Load balancer destination port
+  in.src_port = tcp->dest;   // Load balancer destination port - same as LB source port when redirected!
+  in.dst_port = tcp->source; // Client or Backend source port
   in.protocol = IPPROTO_TCP; // TCP protocol
 
   struct bpf_fib_lookup fib = {};
@@ -242,8 +242,8 @@ int xdp_load_balancer(struct xdp_md *ctx) {
     struct four_tuple_t in_loadbalancer;
     in_loadbalancer.src_ip = ip->daddr;   // Load Balancer IP
     in_loadbalancer.dst_ip = backend->ip; // Backend IP
-    in_loadbalancer.src_port = tcp->dest; // Load Balancer destination port
-    in_loadbalancer.dst_port = tcp->source; // Client source port
+    in_loadbalancer.src_port = tcp->source; // Client source port
+    in_loadbalancer.dst_port = tcp->dest; // Load Balancer destination port
     in_loadbalancer.protocol = IPPROTO_TCP; // TCP protocol 
     struct endpoint client;
     client.ip = ip->saddr; // Client IP
